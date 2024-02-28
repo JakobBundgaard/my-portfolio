7af519bae7f7da5a928bf6b1edce9009eecc3402
JakobBundgaard <jakobbundgaard@hotmail.com>
2024-02-28 13:29:58 +0100

    Make navbar responsive and add hamburger menu for small screens

--- js/script.js
+++ js/script.js
@@ -1,20 +1,27 @@
 const toggle = document.querySelector("#toggleDark");
 const body = document.querySelector("body");
 const heroHeader = document.querySelector(".hero-header");
+const hamburger = document.querySelector(".hamburger");
+const navLinks = document.querySelector(".nav-links");
 
-toggle.addEventListener("click", function () {
-  this.classList.toggle("bi-moon");
-  if (this.classList.toggle("bi-brightness-high-fill")) {
-    body.style.background = "#F5F5F5";
-    body.style.color = "#1c1d25";
-    body.style.transition = "1s";
-  } else {
-    body.style.background = "#000";
-    body.style.color = "#F5F5F5";
-    body.style.transition = "1s";
-  }
-});
+// toggle.addEventListener("click", function () {
+//   this.classList.toggle("bi-moon");
+//   if (this.classList.toggle("bi-brightness-high-fill")) {
+//     body.style.background = "#F5F5F5";
+//     body.style.color = "#1c1d25";
+//     body.style.transition = "1s";
+//   } else {
+//     body.style.background = "#000";
+//     body.style.color = "#F5F5F5";
+//     body.style.transition = "1s";
+//   }
+// });
 
-heroHeader.addEventListener("click", () => {
-  console.log("Hyr mig! I vil ikke fortryde det og jeg kan starte imorgen.");
+document.addEventListener("DOMContentLoaded", function () {
+  hamburger.addEventListener("click", function () {
+    console.log("Clicked");
+    const expanded = this.getAttribute("aria-expanded") === "true" || false;
+    this.setAttribute("aria-expanded", !expanded);
+    navLinks.classList.toggle("show");
+  });
 });
